a8ae44ac020f4918d2a96c1bafd6e85431cf78bb
John Safwat <john362sss@gmail.com>
2023-08-08 10:47:04 +0300

    add updates

---
+++ src/main/kotlin/Kotlin_Summary_File.kt
@@ -0,0 +1,61 @@
+fun main() {
+    println("This is Kotlin Summary")
+    println("\nInKotlin We have Variety of Data Types\n")
+
+    val byteMinimumValue : Byte = Byte.MIN_VALUE
+    val byteMaximumValue : Byte = Byte.MAX_VALUE
+
+    println("The Byte Minimum Value is $byteMinimumValue")
+    println("The Byte Maximum Value is $byteMaximumValue")
+    println()
+
+    val shortMinimumValue : Short = Short.MIN_VALUE
+    val shortMaximumValue : Short = Short.MAX_VALUE
+
+    println("The Short Minimum Value is $shortMinimumValue")
+    println("The Short Maximum Value is $shortMaximumValue")
+    println()
+
+    val intMinimumValue : Int = Int.MIN_VALUE
+    val intMaximumValue : Int = Int.MAX_VALUE
+
+    println("The Int Minimum Value is $intMinimumValue")
+    println("The Int Maximum Value is $intMaximumValue")
+    println()
+
+    val longMinimumValue : Long = Long.MIN_VALUE
+    val longMaximumValue : Long = Long.MAX_VALUE
+
+    println("The Long Minimum Value is $longMinimumValue")
+    println("The Long Maximum Value is $longMaximumValue")
+    println()
+
+    val floatMinimumValue : Float = Float.MIN_VALUE
+    val floatMaximumValue : Float = Float.MAX_VALUE
+
+    println("The Float Minimum Value is $floatMinimumValue")
+    println("The Float Maximum Value is $floatMaximumValue")
+    println()
+
+    val doubleMinimumValue : Double = Double.MIN_VALUE
+    val doubleMaximumValue : Double = Double.MAX_VALUE
+
+    println("The Double Minimum Value is $doubleMinimumValue")
+    println("The Double Maximum Value is $doubleMaximumValue")
+    println()
+
+    val boolMinimumValue : Boolean = false
+    val boolMaximumValue : Boolean = true
+
+    println("The Boolean Minimum Value is $boolMinimumValue")
+    println("The Boolean Maximum Value is $boolMaximumValue")
+    println()
+
+    val charMinimumValue : Char = Char.MIN_VALUE
+    val charMaximumValue : Char = Char.MAX_VALUE
+
+    println("The Char Minimum Value is $charMinimumValue")
+    println("The Char Maximum Value is $charMaximumValue")
+    println()
+
+}
