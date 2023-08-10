b2f87a3466e6709893210109116a9971484c8c42
John Safwat <john362sss@gmail.com>
2023-08-10 04:24:25 +0300

    update

--- src/main/kotlin/Question5.kt
+++ src/main/kotlin/Question5.kt
@@ -1,4 +1,43 @@
 fun main() {
+    println("The Maximum Value is ${getMax(1.1,3.3,4.5,2.3,2.3,4.2,2.4,3.3)}")
+    println("The Minimum Value is ${getMin(1,3,45,23,23,4,2,3)}")
+}
 
+fun getMax(vararg numbers:Int):Int{
+    var max = numbers[0];
+    for(i in numbers.indices-0){
+        if(max < numbers[i]){
+            max = numbers[i]
+        }
+    }
+    return  max
+}
+fun getMax(vararg numbers:Double):Double{
+    var max = numbers[0];
+    for(i in numbers.indices-0){
+        if(max < numbers[i]){
+            max = numbers[i]
+        }
+    }
+    return  max
+}
+
+fun getMin(vararg numbers:Int):Int{
+    var max = numbers[0];
+    for(i in numbers.indices-0){
+        if(max > numbers[i]){
+            max = numbers[i]
+        }
+    }
+    return  max
+}
+fun getMin(vararg numbers:Double):Double{
+    var max = numbers[0];
+    for(i in numbers.indices-0){
+        if(max > numbers[i]){
+            max = numbers[i]
+        }
+    }
+    return  max
 }
 
